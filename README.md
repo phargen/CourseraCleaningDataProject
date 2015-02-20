98eaec5198d41fea5a8835798973288b3b6a9a7e
phargen <phargen.mo@gmail.com>
2015-02-19 23:00:19 -0800

    Add in CodeBook, README, and tidy data producing script

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # CourseraCleaningDataProject
 Repo for Coursera Getting and Cleaning Data project code, February 2015 https://class.coursera.org/getdata-011/
+
+The code in this repository, contained in run_analysis.R combines data from a multi-file data set containing smart phone sensor readings from a variety of basic human activities.  The dataset can be found here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The code combines data extracted from 6 files in particular:
+ * test/subject_test.txt -- holds ID for person wearing smartphone
+ * test/y_test.txt -- holds enumeration value of activity engaged in during measurements
+ * test/X_test.txt -- holds large set of measurements, described as "features" in the original data documentation
+ * train/subject_train.txt
+ * train/y_train.txt
+ * train/X_train.txt
+
+For efficiency, only the features we are interested in (columns with "mean" and "std" data) are ever read from the X_*.txt files.  These measurements are combined with the categorical subject and activity columns, and the test and training data is in turn combined into one large data frame.
+
+After adjusting column names and substituting in meaningful activity names for the enumerated value, the large dataframe is then arranged into groups by activity then subject ID and the mean measurement for each of these subgroups is computed and output to a tidy data text file "getdata-011_projTidy.txt"
+
+The final tidy output follows the following principles:
+1. Each measured variable is in one column
+2. Each different observation (mean per activity/subject grouping) is on a different row
+3. One file per table (just one table, so one file)
+4. Variable names human readable and cleaned of non-alpha characters
+5. Header row at top clearly names columns
+
+The individual measurements are discussed in detail in the accompanying CodeBook.md, including explanations and units.
+
+The tidy data can be read into R with read.table(header=TRUE)
+
+References:
+https://class.coursera.org/getdata-011/forum/thread?thread_id=248
---
+++ run_analysis.R
@@ -0,0 +1,70 @@
+library(plyr)
+
+# Read activity labels and features for later use in providing
+# descriptive activity names and descriptive variable (column) names
+activities <- read.table(file="~/Dataset/activity_labels.txt",
+                         col.names=c("ActivityNum","Activity"), 
+                         colClasses=c("integer", "character"))
+features <- read.table(file="~/Dataset/features.txt",
+                       col.names=c("FeatureNum","Feature"), 
+                       colClasses=c("integer", "character"))
+
+##### Number 4 #####
+# load subject and activity identification columns
+subject_test <- read.table(file="~/Dataset/test/subject_test.txt",
+                           col.names="SubjectNum", colClasses = "integer")
+subject_train <- read.table(file="~/Dataset/train/subject_train.txt",
+                           col.names="SubjectNum", colClasses = "integer")
+activity_test <- read.table(file="~/Dataset/test/y_test.txt",
+                           col.names="ActivityNum", colClasses = "integer")
+activity_train <- read.table(file="~/Dataset/train/y_train.txt",
+                            col.names="ActivityNum", colClasses = "integer")
+
+##### Number 2 & 4 #####
+# determine which feature columns contain mean and std and prepare to read just those columns
+colToKeep <- grepl("mean|std",features[[2]], ignore.case=TRUE)
+#colToKeepNames <- grep("mean|std",features[[2]], ignore.case=TRUE, value = TRUE) 
+# not needed because need to name columns even if not read
+classes <- sapply(colToKeep, FUN = function(x) { if(x) "numeric" else "NULL" })
+
+# read feature data from cases files
+cases_test <- read.table(file="~/Dataset/test/X_test.txt",
+                         col.names=features[[2]], colClasses = classes)
+cases_train <- read.table(file="~/Dataset/train/X_train.txt",
+                            col.names=features[[2]], colClasses = classes)
+
+##### Number 1 #####
+# combine all the above data frames into one large data frame
+# first cbind the test and train data together separately
+all_test <- cbind(activity_test, subject_test, cases_test)
+all_train <- cbind(activity_train, subject_train, cases_train)
+# then rbind the test and train data together
+all_combined <- rbind(all_test, all_train)
+
+##### Number 3 #####
+# change activity number to human readable activity names (factors) and rename column
+all_combined$ActivityNum = as.factor(all_combined$ActivityNum)
+all_combined$ActivityNum = sapply(all_combined$ActivityNum, FUN = function(x) { activities[x, 2] })
+names(all_combined)[names(all_combined)=="ActivityNum"] <- "Activity"
+
+##### Number 4 #####
+newHeaders <- gsub("[. ]", "", names(all_combined)) #remove spaces and '.' from column names
+newHeaders <- gsub("tBodyAcc", "Acceleration", newHeaders) #perform other name substitutions
+newHeaders <- gsub("tGravityAcc", "GravityAcceleration", newHeaders)
+newHeaders <- gsub("tBody", "", newHeaders)
+newHeaders <- gsub("fBodyBodyAcc", "fftAcceleration", newHeaders)
+newHeaders <- gsub("fBodyBody", "fft", newHeaders)
+newHeaders <- gsub("fBodyAcc", "fftAcceleration", newHeaders)
+newHeaders <- gsub("fBody", "fft", newHeaders)
+newHeaders <- gsub("fBody", "", newHeaders)
+newHeaders <- gsub("mean", "Mean", newHeaders)
+newHeaders <- gsub("std", "StandardDeviation", newHeaders)
+newHeaders <- gsub("gravity", "Gravity", newHeaders)
+names(all_combined) <- make.names(newHeaders, unique=TRUE) #remove duplicates if exist
+
+##### Number 5 #####
+all_combined <- arrange(all_combined, Activity, SubjectNum)
+averageOfEachFeaturePerActivityAndSubject <- ddply(all_combined, .(Activity, SubjectNum), colwise(mean))
+
+write.table(averageOfEachFeaturePerActivityAndSubject,
+            file="~/Dataset/getdata-011_projTidy.txt", row.names=FALSE)
